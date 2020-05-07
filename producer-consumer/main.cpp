aea357a33d3cc47a12564efae761051ad7611498
mervedadas <mervedadas96@gmail.com>
2020-05-07 12:27:17 +0300

    have some problems

--- producer-consumer/main.cpp
+++ producer-consumer/main.cpp
@@ -6,13 +6,16 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
+
     std::thread th1(producer1);
-    std::thread th2(consumer1);
-    th1.join();
-    th2.join();
     std::thread th3(producer2);
-    std::thread th4(consumer2);
+    th1.join();
     th3.join();
+
+   // print();
+    std::thread th2(consumer1);
+    th2.join();
+    std::thread th4(consumer2);
     th4.join();
 
 
--- producer-consumer/producer-consumer.cpp
+++ producer-consumer/producer-consumer.cpp
@@ -1,18 +1,22 @@
 #include <QDebug>
 #include <mutex>
 #include <queue>
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
 
 
 std::queue<int> q;
 std::mutex mutex;
 
+
 void producer1(){
     int product;
     srand (time(NULL));
-
-
     mutex.lock();
+
     for(int i =0 ; i<10;i++){
+
         product = rand() % 100 + 1;
         q.push(product);
 
@@ -21,28 +25,14 @@ void producer1(){
 
 }
 
-void consumer1(){
-    int count=0;
-    mutex.lock();
-
-    while(q.size()!=0){
-        if(count!=10){
-            qDebug()<<q.front() + 2;
-            q.pop();
-        }
-        count++;
-    }
-
-    mutex.unlock();
-}
-
-
 void producer2(){
     int product;
-    srand (time(NULL));
 
+    srand (time(NULL));
     mutex.lock();
+
     for(int i =0 ; i<10;i++){
+
         product = rand() % 500 + 200;
         q.push(product);
 
@@ -51,12 +41,30 @@ void producer2(){
 
 }
 
+
+void consumer1(){
+    int count=0;
+    mutex.lock();
+
+   while(q.size()!=10){
+        if(count!=0){
+            qDebug()<<q.front() + 2;
+            q.push(q.front()+2);
+            q.pop();
+
+        }
+        count++;
+    }
+
+    mutex.unlock();
+}
+
 void consumer2(){
     int count=0;
     mutex.lock();
 
-    while(q.size()!=0){
-        if(count!=10){
+   while(q.size()!=10){
+        if(count!=1){
             qDebug()<<q.front() * 10;
             q.pop();
         }
@@ -64,4 +72,12 @@ void consumer2(){
     }
 
     mutex.unlock();
+}
+
+void print(){
+    for(int i=0;i<20;i++){
+        qDebug()<<q.front();
+        q.pop();
+    }
+    qDebug()<<"----------------------------";
 }
